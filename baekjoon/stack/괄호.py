723477a2bd08560d671cdfe3b7ec5a95e9bbb8e0
5onchangwoo <scw9410@gmail.com>
2022-03-03 15:33:43 +0900

    # 블로그: https://teching.tistory.com/

---
+++ baekjoon/stack/괄호.py
@@ -0,0 +1,20 @@
+# 문제: https://www.acmicpc.net/problem/1104
+# 해설: https://teching.tistory.com/
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+for _ in range(n):
+    ps = sys.stdin.readline().rstrip()
+    stack = []
+    for s in ps:
+        if s == '(':
+            stack.append('(')
+        if s == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+    if stack:
+        print("NO")
+    else:
+        print("YES")
