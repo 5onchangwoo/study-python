7a65faab8bd8e3ae472ba1d3a8410682195d5dd3
5onchangwoo <scw9410@gmail.com>
2022-05-03 16:32:17 +0900

    # 블로그: https://teching.tistory.com/

---
+++ baekjoon/문자열/ROT13.py
@@ -0,0 +1,30 @@
+# 문제: https://www.acmicpc.net/problem/4446
+# 블로그: https://teching.tistory.com/
+# A: 65 ~ Z: 90
+# a: 97 ~ z: 122
+import sys
+
+vowel = ['a', 'i', 'y', 'e', 'o', 'u']
+con = ['b', 'k', 'x', 'z', 'n', 'h', 'd', 'c', 'w', 'g', 'p', 'v', 'j', 'q', 't', 's', 'r', 'l', 'm', 'f']
+
+answer = ''
+for line in sys.stdin:
+    for c in line:
+        if c.isalpha():
+            isUp = c.isupper()
+            res = ''
+            if isUp:
+                c = c.lower()
+            if c in vowel:
+                res = vowel[(vowel.index(c)+3)%6]
+            elif c in con:
+                res = con[(con.index(c)+10)%20]
+            if isUp:
+                res = res.upper()
+            answer += res
+        else:
+            answer += c
+print(answer)
+
+
+
