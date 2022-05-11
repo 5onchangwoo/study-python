bda34b5dcb6d8488e89b5cd69cd3b2b287bc5839
5onchangwoo <scw9410@gmail.com>
2022-05-11 17:02:44 +0900

    # 블로그: https://teching.tistory.com/

---
+++ codility/!!!푸는중!!!/A binary gap/solution.java
@@ -0,0 +1,19 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int N) {
+        // write your code in Java SE 8
+    }
+
+    public int binary(int N){
+        while(N <= 1){
+            int j;
+            j = N%2;
+
+        }
+    }
+}
