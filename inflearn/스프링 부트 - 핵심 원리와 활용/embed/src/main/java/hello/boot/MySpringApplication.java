6938df522f987ed0abb6ec532e8d02459a160d76
codeyaki <scw9410@gmail.com>
2024-05-29 18:52:41 +0900

    inflearn: 스프링부트-핵심원리와활용
    - 섹션2: 스프링 부트와 내장 톰캣(ing)

---
+++ inflearn/스프링 부트 - 핵심 원리와 활용/embed/src/main/java/hello/boot/MySpringApplication.java
@@ -0,0 +1,41 @@
+package hello.boot;
+
+import hello.spring.HelloConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.List;
+
+public class MySpringApplication {
+    public static void run(Class ConfigClass, String[] args) {
+        System.out.println("MySpringApplication.run args=" + List.of(args));
+
+        //톰캣 설정
+        Tomcat tomcat = new Tomcat();
+        Connector connector = new Connector();
+        connector.setPort(8888);
+        tomcat.setConnector(connector);
+
+        //서블릿 등록
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(ConfigClass);
+
+        //스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(appContext);
+
+        //디스패처 서블릿 등록
+        Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "dispatcher", dispatcherServlet);
+        context.addServletMappingDecoded("/", "dispatcher");
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
